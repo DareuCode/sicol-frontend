945c10b5574bed6a3d3a07a7afb31c6bc90a3de3
Elmer Izarra <eizarrac@uni.pe>
2024-11-23 17:01:00 -0500

    feat: Configuración de toolkit flowbite con tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from "./component/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sicol-frontend';
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
